fae3d7b8cd345a65a732acf2551fc1b0335b9e2b
Justin Griffith <justin.dean.griffith@gmail.com>
2014-04-23 22:23:25 -0500

    almost done! Adding nested objects

--- src/Rest/Bundle/Classes/DoctrineValueObject.php
+++ src/Rest/Bundle/Classes/DoctrineValueObject.php
@@ -55,14 +55,61 @@ class DoctrineValueObject {
 
     $this->getMetaForClass();
 
+    $this->getFieldMappings();
+    $this->getAssociationMappings();
+
+    return $this->valueObject;
+
+  }
+
+  public function getFieldMappings() {
+
     foreach($this->meta->fieldMappings as $key => $field) {
 
       $function = "get" . ucwords($key);
-      $this->valueObject->$key = $entity->$function();
+      $this->valueObject->$key = $this->entity->$function();
 
     }
 
-    return $this->valueObject;
+  }
+
+  public function getAssociationMappings() {
+
+    foreach($this->meta->associationMappings as $key => $assocation) {
+
+      $function = "get" . ucwords($key);
+      $this->valueObject->$key = $this->entity->$function();
+
+      // If collection convert to array
+      if (preg_match("/Doctrine(.*)Collection/", get_class($this->valueObject->$key))) {
+        $this->valueObject->$key = $this->valueObject->$key->toArray();
+      }
+      else {
+
+        $name = $key;
+        $entity = $this->valueObject->$key;
+        $meta = $this->cmf->getMetadataFor(get_class($entity));
+        $valueObject = new \stdClass();
+
+        foreach($meta->fieldMappings as $key => $field) {
+
+          $function = "get" . ucwords($key);
+          $valueObject->$key = $entity->$function();
+
+        }
+
+        foreach($meta->associationMappings as $key => $field) {
+
+          $function = "get" . ucwords($key);
+          $valueObject->$key = $entity->$function();
+
+        }
+
+        $this->valueObject->$name = $valueObject;
+
+      }
+
+    }
 
   }
 
